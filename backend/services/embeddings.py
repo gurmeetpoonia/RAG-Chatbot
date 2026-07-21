209d6f838d418bfa3f303686f0d2439f3cfd8702
Gurmeet Punia <gurmeetpunia404@gmail.com>
2026-07-21 16:49:08 +0530

    code updated

--- backend/services/embeddings.py
+++ backend/services/embeddings.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import os
 
@@ -6,10 +7,10 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_embedding(text: str):
+def get_embedding(text: str, task_type: str = "RETRIEVAL_DOCUMENT"):
     response = client.models.embed_content(
         model="text-embedding-004",
-        contents=text
+        contents=text,
+        config=types.EmbedContentConfig(task_type=task_type)
     )
-
     return response.embeddings[0].values
